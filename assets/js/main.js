af1ca5a85bc506c63fb7c0ed8c4f07b4ba523c29
GiovannyQuiroz <giovanny.quiroz94@gmail.com>
2022-05-27 09:34:31 -0500

    Actualizacion icono scroll top

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,9 @@ $(document).ready(function(){
     $(".hamburguer").click(function() {
         $(".hamburguer span").toggleClass("active");
         $(".inside-responsiveMenu ").slideToggle();
+        if($(".hamburguer span").hasClass("active")) {
+            $(".inside-responsiveMenu").css("z-index","20")
+        }
     })
     $(".modulos-resp").click(function() {
         $(".sub-sub-Menu").slideToggle();
